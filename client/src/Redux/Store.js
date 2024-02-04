11a4270ff555df8da97570d2ba5360b8df9dcbc2
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2024-02-04 12:50:07 +0530

    redux folder structure is saved

---
+++ client/src/Redux/Store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userSlice from './Reducer';
+
+import logger from 'redux-logger';
+
+export default configureStore({
+  reducer: {
+    user: userSlice,
+    middleware: [logger],
+  },
+});
