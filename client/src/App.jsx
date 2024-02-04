32184828d48821585ed2dca39d54cffa0a62b89e
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2024-02-04 12:47:50 +0530

    app files are added

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { BrowserRouter as Router} from "react-router-dom";
+import CustomRoutes from "./routes/CustomRoutes";
+import { UserContext } from "./utils/userContext";
+import { useState } from "react";
+
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <UserContext.Provider value={[user, setUser]}>
+      <Router>
+        <CustomRoutes />
+      </Router>
+    </UserContext.Provider>
+  );
+};
+
+export default App;
