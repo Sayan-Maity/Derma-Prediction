26bed053777479ac2628b702fff36db3af2dd46f
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2024-02-10 20:17:06 +0530

    added check2 route

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { register, check, login } = require("../controllers/AuthController");
+const { register, check, login, check2 } = require("../controllers/AuthController");
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/check", check);
+router.post("/check2", check2);
 router.post("/login", login);
 
 module.exports = router;
