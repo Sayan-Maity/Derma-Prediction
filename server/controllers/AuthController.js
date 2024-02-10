6a41265abe23e59457a60444af84f17cde25934d
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2024-02-10 20:12:14 +0530

    made two check controllers for login and register

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,8 +1,9 @@
-const { Magic } = require('@magic-sdk/admin');
-const User = require('../models/UserModel');
-require('dotenv').config();
+const { Magic } = require("@magic-sdk/admin");
+const User = require("../models/UserModel");
+require("dotenv").config();
 
 const magic = new Magic(process.env.MAGIC_SECRET_KEY);
+
 
 //register user
 module.exports.register = async (req, res) => {
@@ -12,7 +13,7 @@ module.exports.register = async (req, res) => {
       email,
     });
     if (user) {
-      return res.status(400).send('User already exists');
+      return res.status(400).send("User already exists");
     }
     const newUser = new User({
       email,
@@ -21,7 +22,29 @@ module.exports.register = async (req, res) => {
       role,
     });
     await newUser.save();
-    return res.status(201).send('User created successfully');
+    return res.status(201).send("User created successfully");
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+// check if user email exists in database
+module.exports.check2 = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await User.findOne({
+      email,
+    });
+    if (!user) {
+      return res.status(200).json({
+        status: false,
+      });
+    }
+    return res.status(200).json({
+      status: true,
+    });
   } catch (error) {
     return res.status(500).json({
       error: error.message,
@@ -32,19 +55,17 @@ module.exports.register = async (req, res) => {
 // check if user email exists in database
 module.exports.check = async (req, res) => {
   const { email } = req.body;
-  const doctorEmail = process.env.DOCTOR_EMAIL;
-
   try {
     const user = await User.findOne({
       email,
     });
     if (!user) {
-      return res.status(400).json({
+      return res.status(200).json({
         status: false,
       });
     } else {
-      if (email === doctorEmail) {
-        return res.status(400).json({
+      if (user.role && user.role === "doctor") {
+        return res.status(200).json({
           status: true,
           doctor: true,
         });
@@ -67,14 +88,14 @@ module.exports.login = async (req, res) => {
   try {
     const didToken = req.headers.authorization.substring(7);
     await magic.token.validate(didToken);
-    console.log('User is authenticated');
+    console.log("User is authenticated");
     const issuer = await magic.token.getIssuer(didToken);
     console.log(issuer);
     const user = await User.findOne({
       email: req.body.email,
     });
     if (!user) {
-      return res.status(400).send('User does not exist');
+      return res.status(400).send("User does not exist");
     }
     //update the magicId
     user.magicId = issuer;
