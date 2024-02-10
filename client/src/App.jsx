012b84b6dd02cdf28df02de6d075ef196ce3d9e8
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2024-02-10 21:04:28 +0530

    integrated hotjar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,18 @@
 import { BrowserRouter as Router} from "react-router-dom";
 import CustomRoutes from "./routes/CustomRoutes";
 import { UserContext } from "./utils/userContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { hotjar } from "react-hotjar";
 
 const App = () => {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    hotjar.initialize(
+      process.env.REACT_APP_HOTJAR_SITEID,
+      process.env.REACT_APP_HOTJAR_VERSION
+    );
+  }, []);
 
   return (
     <UserContext.Provider value={[user, setUser]}>
