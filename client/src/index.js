88e93f8e193a24228fe722234c47d6e82386a0b9
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2024-02-17 18:07:56 +0530

    removed bootstrap.min.css file

--- client/src/index.js
+++ client/src/index.js
@@ -9,8 +9,6 @@ import App from './App';
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 const theme = extendTheme({
   colors: {
     dashboard: {
