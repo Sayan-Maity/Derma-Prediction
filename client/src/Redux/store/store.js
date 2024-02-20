cf6e77318b3637c480f3bae169570e0a3a5a2af4
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2024-02-20 17:39:04 +0530

    removed unwanted path import

--- client/src/Redux/store/store.js
+++ client/src/Redux/store/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../reducers/userSlice';
 import dermaDetectionReducer from '../reducers/dermaDetectionSlice';
-import dermaInfoReducer from '../reducers/dermaInfoSlice';
 import logger from 'redux-logger';
 
 export default configureStore({
