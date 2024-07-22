1d9a829c3ae9b4f6b5b5823ad7406a42222797ad
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2024-07-23 02:17:42 +0530

    constant for health analytics accordion item

---
+++ client/src/constants/HealthAnalyticsAccordionItems.jsx
@@ -0,0 +1,18 @@
+export const HealthAnalyticsAccordionItems = [
+    {
+        title: "Lifestyle Analytics",
+        content: "Track and analyze your daily lifestyle habits, including physical activity, sleep patterns, and general well-being. By inputting your daily routines and activities, you can get insights into your overall lifestyle and make informed decisions to improve your health and quality of life."
+    },
+    {
+        title: "Water Intake Analytics",
+        content: "Monitor your daily water consumption to ensure you stay properly hydrated. Input the amount of water you drink each day, and get detailed analytics on your hydration levels. Staying hydrated is crucial for maintaining bodily functions, improving energy levels, and enhancing overall health."
+    },
+    {
+        title: "Diet Analytics",
+        content: "Analyze your daily food intake to maintain a balanced and nutritious diet. Enter the types and amounts of food you consume each day, and receive comprehensive analytics on your dietary habits. This will help you understand your nutritional intake and make healthier food choices."
+    },
+    {
+        title: "Skin Analytics",
+        content: "Keep track of your skincare routine and the effects of different products on your skin. Record your daily skincare activities, including product usage and any skin reactions. Get insights into how your skincare regimen impacts your skin health, and adjust your routine for better results."
+    }
+];
