921e326607528ab0e3c0e62422841a0ae9965024
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2024-07-23 14:16:51 +0530

    features for gold and basic plan

---
+++ client/src/constants/UpgradePlanBenifits.jsx
@@ -0,0 +1,12 @@
+export const BasicPlanBenifits = [
+    "Get 1 consultation",
+    "Rs. 500 only per consultation",
+]
+
+export const GoldPlanBenifits = [
+    "Get 12 consultations",
+    "Rs. 3000 only for 6 months",
+    "Access to premium features",
+    "Get notified early for new features",
+
+]
